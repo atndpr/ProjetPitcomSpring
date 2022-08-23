2247ae486ec3dcbdf4998686d043158d2b5a066a
Anthony DELPIERRE <anthony.delpierre@icloud.com>
2022-08-23 13:33:53 +0200

    Initial commit

---
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/ProjetPitcomSpringApplication.java
@@ -0,0 +1,13 @@
+package com.orsys.projet.sa.projetpitcomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPitcomSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetPitcomSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orsys/projet/sa/projetpitcomspring/ProjetPitcomSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orsys.projet.sa.projetpitcomspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPitcomSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
