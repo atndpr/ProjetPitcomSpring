c32cf2dc705bfa492bf4e776a230a878c80042ef
Anthony DELPIERRE <anthony.delpierre@icloud.com>
2022-08-23 13:38:20 +0200

    adds anthony business classes

---
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/business/Zone.java
@@ -0,0 +1,25 @@
+package com.orsys.projet.sa.projetpitcomspring.business;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Zone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @ManyToOne
+    private Annonce annonce;
+    @OneToMany(mappedBy = "zone", cascade = CascadeType.REMOVE)
+    private List<Arret> arrets;
+}
