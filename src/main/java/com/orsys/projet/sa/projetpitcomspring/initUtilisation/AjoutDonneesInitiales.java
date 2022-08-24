cd356bfdef0c01f54495fb83ba6211e6928cf862
Sulaiman1015 <sulaiman1015@gmail.com>
2022-08-24 11:18:25 +0200

    add files inits

--- src/main/java/com/orsys/projet/sa/projetpitcomspring/initUtilisation/AjoutDonneesInitiales.java
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/initUtilisation/AjoutDonneesInitiales.java
@@ -1,4 +1,34 @@
 package com.orsys.projet.sa.projetpitcomspring.initUtilisation;
 
-public class AjoutDonneesInitiales {
+import com.orsys.projet.sa.projetpitcomspring.business.Client;
+import com.orsys.projet.sa.projetpitcomspring.business.Utilisateur;
+import com.orsys.projet.sa.projetpitcomspring.dao.UtilisateurDao;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AjoutDonneesInitiales implements CommandLineRunner {
+
+    private final UtilisateurDao utilisateurDao;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        ajouterUtilisateurs();
+    }
+
+    private void ajouterUtilisateurs() {
+        if(utilisateurDao.count()==0){
+            Client client = new Client();
+            client.setNom("MMT");
+            client.setPrenom("Sulaiman");
+            client.setEmail("client1@orsys.fr");
+            client.setMotDePassse("12345678");
+            client.setNumeroDeTelephone("0627216923");
+            utilisateurDao.save(client);
+        }
+        utilisateurDao.findAll();
+    }
 }
