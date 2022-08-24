58d260536eb76812320105b587736edc1cf7a261
Anthony DELPIERRE <anthony.delpierre@icloud.com>
2022-08-24 09:28:34 +0200

    anthony services

--- src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/AnnonceServiceImpl.java
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/AnnonceServiceImpl.java
@@ -1,6 +1,12 @@
 package com.orsys.projet.sa.projetpitcomspring.service.impl;
 
+import com.orsys.projet.sa.projetpitcomspring.dao.ArretDao;
 import com.orsys.projet.sa.projetpitcomspring.service.AnnonceService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class AnnonceServiceImpl implements AnnonceService {
+    private ArretDao arretDao;
 }
--- src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/DiffusionServiceImpl.java
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/DiffusionServiceImpl.java
@@ -1,6 +1,12 @@
 package com.orsys.projet.sa.projetpitcomspring.service.impl;
 
+import com.orsys.projet.sa.projetpitcomspring.dao.DiffusionDao;
 import com.orsys.projet.sa.projetpitcomspring.service.DiffusionService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class DiffusionServiceImpl implements DiffusionService {
+    private DiffusionDao diffusionDao;
 }
