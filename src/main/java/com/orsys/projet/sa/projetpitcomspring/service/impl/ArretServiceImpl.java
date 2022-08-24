b53c2df72fb05697a632068391d1dde0bc01ef13
Anthony DELPIERRE <anthony.delpierre@icloud.com>
2022-08-24 09:45:58 +0200

    fixes mistake from anthony services

--- src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/ArretServiceImpl.java
+++ src/main/java/com/orsys/projet/sa/projetpitcomspring/service/impl/ArretServiceImpl.java
@@ -1,6 +1,5 @@
 package com.orsys.projet.sa.projetpitcomspring.service.impl;
-
-import com.orsys.projet.sa.projetpitcomspring.dao.AnnonceDao;
+import com.orsys.projet.sa.projetpitcomspring.dao.ArretDao;
 import com.orsys.projet.sa.projetpitcomspring.service.ArretService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +7,5 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class ArretServiceImpl implements ArretService {
-    private AnnonceDao annonceDao;
+    private ArretDao arretDao;
 }
